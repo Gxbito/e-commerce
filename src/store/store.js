7dc427c7c031257f72a83e95fbb9717065cf7c6e
Gxbito <g4bmz02@gmail.com>
2024-11-30 19:59:48 -0300

    arreglos de importacion en el store

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,12 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../features/userSlice/userSlice';
-import cartReducer from '../features/CartSlice/cartSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../features/userSlice/userSlice"; // Arreglo de importacion
+import cartReducer from "../features/CartSlice/cartSlice";
 import checkoutReducer from "../features/CheckoutSlice/checkoutSlice";
 
 const persistCartMiddleware = (store) => (next) => (action) => {
   const result = next(action);
   const state = store.getState();
-  localStorage.setItem('cart', JSON.stringify(state.cart.items));
+  localStorage.setItem("cart", JSON.stringify(state.cart.items));
   return result;
 };
 
